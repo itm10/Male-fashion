5ddad6c7a484a58d61ea403c4e890b372daa9373
itm10 <ibrohim.islom10@hmail.com>
2024-03-25 20:12:29 +0500

    Done

--- root/settings.py
+++ root/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x2c+^7diim+97$ymq2qgfhfy@p5etn02=&c+*%z-%c19&@+$++'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -61,7 +61,6 @@ MIDDLEWARE = [
 
 CSRF_TRUSTED_ORIGINS = [
     'https://1cb3-84-54-90-175.ngrok-free.app',
-    # Add other trusted origins if needed
 ]
 
 ROOT_URLCONF = 'root.urls'
